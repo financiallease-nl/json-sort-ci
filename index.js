8ac9d04d314f5fe598e7a1ca49d87359324722f2
Rik de Groot <hwdegroot@gmail.com>
2023-01-27 11:21:19 +0100

    Improve output from ordering
    * Colorize the output
    * Number the files
    * Compress output
    * Exit with 1 on dryRun failure
    * Add summary at the end
    * Add option to determine the indentation
    * Add option to only show the unsorted files in the output

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ yargs(hideBin(process.argv))
     .command('$0 <file> [dry-run][excludes]', 'sort json alphabetically', async(yargs) => {
         const { argv } = yargs;
         let files = argv["_"];
-
         if (files) {
 
             let {
                 dryRun,
+                unsortedOnly,
+                indent,
                 excludes,
             } = argv;
 
@@ -24,34 +25,49 @@ yargs(hideBin(process.argv))
                 files = files.filter( file => !excludes.includes(/[^/]*$/.exec(file)[0]));
             }
 
-            let errors = 0;
-            await Promise.allSettled(files.map(async(file) => {
-                let jsonFile = await fs.readFileSync(file);
+            let unsortedFiles = [];
+            let index = 0;
+            await Promise.all(files.map(file => {
+                index += 1;
+                let jsonFile = fs.readFileSync(file);
                 let jsonContent = JSON.parse(jsonFile);
                 let sortedJsonContent = sortJson(jsonContent, {});
 
                 // No CI check mode. Just sort and update the given json file locally.
+                const fileName = `${file}`.bold.dim;
                 if (dryRun === false) {
                     if (JSON.stringify(jsonContent) !== JSON.stringify(sortedJsonContent)) {
-                        throw new Error(`Something went wrong sorting ${file}`.red);
+                        console.info(`[${index}] ${fileName} sorted`.yellow);
+                        let formattedContent = JSON.stringify(sortedJsonContent, null, indent);
+                        fs.writeFileSync(file, formattedContent)
+                        unsortedFiles.push(file);
+                    } else if (!unsortedOnly) {
+                        console.info(`[${index}] ${fileName} is already sorted`.green);
                     }
-                    console.info(`${file} is sorted alphabetically!`.green);
-                    return 0;
+                    return;
                 }
 
                 // CI check mode. Compare sorted file and local file.
                 if (JSON.stringify(jsonContent) !== JSON.stringify(sortedJsonContent)) {
-                    console.error(`${file} not sorted alphabetically`.red);
-                    errors = errors + 1;
-                } else {
-                    console.info(`${file} is already sorted alphabetically`.green);
+                    console.error(`[${index}] ${fileName} not sorted`.red);
+                    unsortedFiles.push(file);
+                } else if (!unsortedOnly) {
+                    console.info(`[${index}] ${fileName} is sorted`.green);
                 }
             }));
 
-            if (errors > 0) {
-                throw new Error(`${errors} file(s) not ordered alphabetically`.red);
+
+            if (unsortedFiles.length > 0) {
+                if (dryRun) {
+                    console.info(`\n${unsortedFiles.length} of ${files} file(s) not sorted`.red);
+                     process.exit(1);
+                }
+                console.info(`\n${unsortedFiles.length} files sorted`.yellow.bold);
+                process.exit(0);
             }
-            return errors;
+
+            console.info(`\nAll files (${files.length}) are sorted`.green.bold);
+            process.exit(0);
         }
 
         return yargs.showHelp();
@@ -59,6 +75,18 @@ yargs(hideBin(process.argv))
     .positional("file", {
         type: "array",
         required: true,
+    })
+    .option('indent', {
+        alias: 'i',
+        type: 'integer',
+        description: 'indents to apply when the file is formatted',
+        default: 4,
+    })
+    .option('unsorted-only', {
+        alias: 'u',
+        type: 'boolean',
+        description: 'only show output for unsorted files',
+        default: false,
     })
     .option('dry-run', {
         alias: 'dr',
